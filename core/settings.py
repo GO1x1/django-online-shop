f38cfadec8a95357e55b95337624ec21074aa997
Mosunov Danil <dvmosunov@mail.ru>
2023-07-22 14:17:33 +0300

    add database

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'OnlineShop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Ropojon77!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
